2e4f135c490372db773488a11376e709920c68dc
Tim Sundell <tim920129@hotmail.com>
2023-09-19 09:30:10 +0200

    :fire: Removed unused functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,11 @@ import { DigiButton } from '@digi/arbetsformedlingen-react'
 import './App.css'
 import { Form } from './components/Form'
 import Header from './components/Header'
-import {
-  getCompetenciesByOccupationId,
-  postOccupationMatchesByText,
-} from './services/AFservice'
+import {getCompetenciesByOccupationId } from './services/AFservice'
 import * as AF from '@digi/arbetsformedlingen'
 
 function App() {
-  const searchMatches = async () => {
-    try {
-      const result = await postOccupationMatchesByText({
-        input_text: 'frontend',
-        input_headline: 'utvecklare',
-        limit: 10,
-        offset: 0,
-        include_metadata: false,
-      })
-
-      console.log(result)
-    } catch (error) {
-      console.error('Error:', error)
-    }
-  }
-
+ 
   const searchCompetencies = async () => {
     try {
       const result = await getCompetenciesByOccupationId({
@@ -36,18 +18,11 @@ function App() {
       console.error('Error:', error)
     }
   }
+  
   return (
     <>
       <Header />
       <Form />
-      <DigiButton
-        afSize={AF.ButtonSize.MEDIUM}
-        afVariation={AF.ButtonVariation.PRIMARY}
-        afFullWidth={false}
-        onClick={searchMatches}
-      >
-        POST occupations - se konsolen för resultat
-      </DigiButton>
       <DigiButton
         afSize={AF.ButtonSize.MEDIUM}
         afVariation={AF.ButtonVariation.PRIMARY}
