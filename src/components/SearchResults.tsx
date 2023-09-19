ffbcea5e8e0cd92ba8ac426d5cc949607f4f4b4c
Tim Sundell <tim920129@hotmail.com>
2023-09-19 18:54:21 +0200

    :ambulance: Improved rangeBar and made it compatible with searchResults

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -8,7 +8,6 @@ const SearchResults = ({ result }: SearchResultsProps) => {
 
     return (
         <div className='SR-container'>
-            <h3>Träffar: {result.hits_returned}</h3>
             <ul className='SR-matches'>
                 {result.related_occupations.map((occupation, index) => {
                     if(!occupation.occupation_label) {
