bb65b883324199159d263e04f696dbefb565aced
Veronica <veronica.bergstrom@medieinstitutet.se>
2023-09-20 14:43:15 +0200

    validation suggestion and css adjusted to AF standard

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,47 @@
-import { useState } from 'react'
-import { IMatch } from './models/IMatch'
-import './App.css'
-import { Form } from './components/Form'
-import Header from './components/Header'
-import  SearchResults  from './components/SearchResults'
-import RangeBar from './components/RangeBar'
-        
-function App() {
+import { useState } from "react";
+import { IMatch } from "./models/IMatch";
 
+import { Form } from "./components/Form";
+import Header from "./components/Header";
+import SearchResults from "./components/SearchResults";
+import RangeBar from "./components/RangeBar";
+import "./App.css";
+import "./styles/form.css";
+import "./styles/header.css";
+
+function App() {
   const [results, setResults] = useState<IMatch>();
   const [responseData, setResponseData] = useState<IMatch>();
-  
+
   const onSearch = (incomingResult: IMatch): void => {
-    setResults(incomingResult)
-  }
+    setResults(incomingResult);
+  };
 
   const handleResponse = (data: IMatch) => {
-    setResponseData(data)
-  }
+    setResponseData(data);
+  };
 
   const handleRangeChange = (value: number) => {
     if (responseData) {
       setResponseData({ ...responseData, hits_returned: +value });
-      console.log("respons",responseData)
+      console.log("respons", responseData);
       //behöver göra ett api anrop till service?
     }
-  }
+  };
 
   return (
     <>
       <Header />
-      <Form onSearch={onSearch} onSearchMatch={handleResponse}/>
-      {responseData && (<RangeBar responseData={responseData} onRangeChange={handleRangeChange} />)}
-      {results ? <SearchResults result={results} /> : null} 
+      <Form onSearch={onSearch} onSearchMatch={handleResponse} />
+      {responseData && (
+        <RangeBar
+          responseData={responseData}
+          onRangeChange={handleRangeChange}
+        />
+      )}
+      {results ? <SearchResults result={results} /> : null}
     </>
-  )
+  );
 }
 
 export default App;
