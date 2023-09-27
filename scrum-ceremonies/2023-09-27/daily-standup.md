375a62178572e0c319e5020ccce854e160882809
Tim Sundell <tim920129@hotmail.com>
2023-09-27 11:22:36 +0200

    :memo: daily standup

---
+++ scrum-ceremonies/2023-09-27/daily-standup.md
@@ -0,0 +1,18 @@
+# Daily Standup Report 2023-09-27
+    
+# Reflektion gårdagen   
+
+Yesterday I…, today I will…, I need help with…, I'm stuck with…, TIL…
+
+# Tim
+
+Gjort css för page navigation/rangebar + accesibility check, fixar readme.
+
+# Veronica
+
+Gjort css, ska fixa sidan responsiv.
+
+# Mattias
+
+Fixat styling på searchresults, tar bort console loggar och kikar på deployment.
+
--- src/components/FilterContainer.tsx
+++ src/components/FilterContainer.tsx
@@ -9,8 +9,9 @@ interface RangeBarProps {
   responseData: IMatch | undefined;
   onRangeChange: (value: number, startValue: number, endValue: number) => void;
 }
+
 interface IFilterContext {
-  rangeValue: number;
+  rangeValue: number; 
   pagination: IPagination;
   responseData: IMatch | undefined;
   totalPages: number;
@@ -26,39 +27,40 @@ export const FilterContext = createContext<IFilterContext>(
 const FilterContainer = ({ responseData, onRangeChange }: RangeBarProps) => {
   const [rangeValue, setRangeValue] = useState<number>(Math.min(10, responseData?.hits_total || 0));
   const [pagination, setPagination] = useState<IPagination>({
-    currentPage: 1,
-    currentStartValue: 0,
-    currentEndValue: Math.min(9, responseData?.hits_returned || 0),
-    itemsPerPage: 10,
+    //här sätts startvärden för pagineringen
+    currentPage: 1, //vilken sida man börjar på
+    currentStartValue: 1, //första siffran i "visar 1-10"
+    currentEndValue: Math.min(10, responseData?.hits_returned || 0), //väljer det minsta värdet av 9, eller så många resultat vi får tebax, || 0 som dyker upp på flera ställer är bara en backup ifall responseData?.hits_returned är undefined 
+    itemsPerPage: 10, //hur många yrken varje sida ska visa.
   });
 
-  const totalPages = Math.ceil(
+  const totalPages = Math.ceil( //hur många sidor ska det vara? det avrundar resultatet av responseData?.hits_returned /pagination.itemsPerPage som vi satt till 10, så om responseData?.hits_returned är 20 får vi 2 sidor 
     (responseData?.hits_returned || 0) / pagination.itemsPerPage
   );
 
-  useEffect(() => {
+  useEffect(() => { //useffektens syfte är dels att updatera pagineringen i setPagination, men även ändra yrkena som renderas i onRangeChange (som körs i app.tsx)
     const maxResult = responseData?.hits_returned || 0;
-    const newStart = (pagination.currentPage - 1) * pagination.itemsPerPage + 1;
-    const newEnd = Math.min(newStart + pagination.itemsPerPage - 1, maxResult);
+    const newStart = (pagination.currentPage - 1) * pagination.itemsPerPage + 1; //Gör så startvärdet alltid börjar på 1, eller 11, eller 21 osv. (pagination.currentPage - 1) gör att om vi är på sida 2, blir resultatet 1 sen plussar vi på pagination.itemsPerPage + 1 som blir 11a
+    const newEnd = Math.min(newStart + pagination.itemsPerPage - 1, maxResult); 
 
-    setPagination((prevPagination) => ({
+    setPagination((prevPagination) => ({ //uppdaterar paginerings statet
       ...prevPagination,
       currentStartValue: newStart,
       currentEndValue: newEnd,
     }));
 
-    onRangeChange(rangeValue, newStart, newEnd);
+    onRangeChange(rangeValue, newStart, newEnd); //se handleRangeChange  i app.tsx, det uppdaterar resultatet där så SearchResults får rätt info
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [rangeValue, responseData?.hits_returned, pagination.currentPage]);
+  }, [rangeValue, pagination.currentPage]); //den här useeffekten körs om man ändrar värdet på rangebaren, eller byter sida i pagination
 
-  const handleRangeChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setRangeValue(+event.target.value);
-    onRangeChange(
-      rangeValue,
-      pagination.currentStartValue,
-      pagination.currentEndValue
-    );
-    setPagination((prevPagination) => ({
+  const handleRangeChange = (event: ChangeEvent<HTMLInputElement>) => { //triggas varje gång vi rör rangebaren
+    setRangeValue(+event.target.value); //uppdaterar och konverterar statet till number
+    // onRangeChange(  //Har kommenterat ut detta, vet ej om det behövs, körs redan i useeffect?
+    //   rangeValue,
+    //   pagination.currentStartValue,
+    //   pagination.currentEndValue
+    // );
+    setPagination((prevPagination) => ({ //man hamnar automatiskt på sida 1 om man ändrar rangevalue mitt i en sökning
       ...prevPagination,
       currentPage: 1,
     }));
@@ -67,8 +69,8 @@ const FilterContainer = ({ responseData, onRangeChange }: RangeBarProps) => {
   const handlePaginationChange = (
     e: DigiNavigationPaginationCustomEvent<number>
   ) => {
-    const newPage = e.detail;
-    setPagination((prevPagination) => ({
+    const newPage = e.detail; //e.detail är ett number, som är siffran vi befinner oss på, fick forska mig fram till det :P
+    setPagination((prevPagination) => ({ 
       ...prevPagination,
       currentPage: newPage,
     }));
